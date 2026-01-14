1ee08391bf996dddc131ae811101bbb120ce435a
NJcherry <inessaogarkova@mail.ru>
2026-01-14 21:44:24 +0300

    fixed AddToCartTest

--- src/test/java/AddToCartTest.java
+++ src/test/java/AddToCartTest.java
@@ -1,5 +1,6 @@
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -10,56 +11,61 @@ import static com.codeborne.selenide.ScrollIntoViewOptions.instant;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AddToCartTest {
-    private String itemPizza = "Мясное плато";
+    private String pizzaName = "Мясное плато";
+    private String toppingName = "Шампиньоны 20г";
+    private String addToCartButton = "В корзину";
+    private String pizzaBigSize = "Большая";
+    private String pizzaMediumSize = "Средняя";
+    private String textAmountCheck = "В корзине 1 шт.";
 
     @Test
-    void cartTest() {
-        open("https://pizzafabrika.ru/vologda");
+    @DisplayName("Добавление товара в корину")
+    void addPizzaToCartTest() {
+        Configuration.baseUrl = "https://pizzafabrika.ru";
+        open("/vologda");
 
         $("[data-testid='pizza']").click();
 
         $$("#pizza article[role='button']")
-                .findBy(text(itemPizza))
+                .findBy(text(pizzaName))
                 .scrollIntoView((instant().block(center)))
                 .shouldBe(visible).click();
-
-        int bigPrice = Integer.parseInt($$("button").findBy(Condition.text("В корзину")).$("[class*='currency_nowrap'] span").getText());
-        System.out.println(bigPrice);
 
         $$("[class*='switcher_tab-label']")
-                .findBy(Condition.text("Средняя"))
+                .findBy(text(pizzaBigSize))
                 .shouldBe(visible).click();
 
-        int mediumPrice = Integer.parseInt($$("button").findBy(Condition.text("В корзину")).$("[class*='currency_nowrap'] span").getText());
-        System.out.println(mediumPrice);
+        int largeSizePrice = Integer.parseInt($$("button").findBy(text(addToCartButton)).$("[class*='currency_nowrap'] span").getText());
 
-        Assertions.assertTrue(bigPrice > mediumPrice, "Цена на большую пиццу \"" + bigPrice + "\" должна быть больше, чем цена на среднюю \"" + mediumPrice + "\"");
+        $$("[class*='switcher_tab-label']")
+                .findBy(text(pizzaMediumSize))
+                .shouldBe(visible).click();
+
+        int mediumSizePrice = Integer.parseInt($$("button").findBy(text(addToCartButton)).$("[class*='currency_nowrap'] span").getText());
+
+        Assertions.assertTrue(largeSizePrice > mediumSizePrice, "Цена на большую пиццу \"" + largeSizePrice + "\" должна быть больше, чем цена на среднюю \"" + mediumSizePrice + "\"");
 
         $$("li[role='menuitem']")
-                .findBy(text("Шампиньоны 20г"))
+                .findBy(text(toppingName))
                 .scrollIntoView((instant().block(center)))
                 .shouldBe(visible).click();
 
-        int ingredientPrice = Integer.parseInt($$("li[role='menuitem']").findBy(text("Шампиньоны 20г")).$("span [class*='currency_nowrap']").getOwnText().trim());
-        System.out.println(ingredientPrice);
+        int ingredientPrice = Integer.parseInt($$("li[role='menuitem']").findBy(text(toppingName)).$("span [class*='currency_nowrap']").getOwnText().trim());
 
-        int finalPrice = Integer.parseInt($$("button").findBy(Condition.text("В корзину")).$("[class*='currency_nowrap'] span").getText());
-        System.out.println(finalPrice);
+        int finalPrice = Integer.parseInt($$("button").findBy(text(addToCartButton)).$("[class*='currency_nowrap'] span").getText());
 
-        Assertions.assertEquals(finalPrice, mediumPrice + ingredientPrice, "Итоговая сумма должна увеличиться на цену топпинга");
+        Assertions.assertEquals(finalPrice, mediumSizePrice + ingredientPrice, "Итоговая сумма должна увеличиться на цену топпинга");
 
         $$("[class*='editor-buttons_editor']")
-                .findBy(Condition.text("В корзину"))
+                .findBy(text(addToCartButton))
                 .shouldBe(visible).click();
 
         $$("[class*='buy-button_small']")
-                .findBy(Condition.text("В корзине 1 шт."))
+                .findBy(text(textAmountCheck))
                 .shouldBe(visible);
 
         actions().sendKeys(Keys.ESCAPE).perform();
 
-        $("[class*='entry-card_info']").shouldHave(text("Мясное плато средняя"), text("Шампиньоны 20г"), text(String.valueOf(finalPrice)));
-
-
+        $("[class*='entry-card_info']").shouldHave(text(pizzaName + " " + pizzaMediumSize), text(toppingName), text(String.valueOf(finalPrice)));
     }
 }
