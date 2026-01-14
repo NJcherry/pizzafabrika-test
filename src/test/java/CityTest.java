495407fdda50dacb46e503908f65db4d92f1693c
NJcherry <inessaogarkova@mail.ru>
2026-01-14 21:51:15 +0300

    added CityTest

---
+++ src/test/java/CityTest.java
@@ -0,0 +1,67 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.ScrollIntoViewOptions.Block.center;
+import static com.codeborne.selenide.ScrollIntoViewOptions.instant;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CityTest {
+    private String cityName = "Вологда";
+    private String itemPizza = "Мясное плато";
+
+    @Test
+    void cityTest() {
+        open("https://pizzafabrika.ru");
+
+        $$("[class*='list_container'] button")
+                .findBy(Condition.text(cityName))
+                .click();
+
+        $("button [class*='current-user-location']").shouldHave(text(cityName));
+
+        refresh();
+
+        $("button [class*='current-user-location']").shouldHave(text(cityName));
+
+        $("[data-testid='delivery-logic_unknown-card']").click();
+
+        $("[class*='polygon-info_title']").shouldHave(text(cityName));
+
+        $("[class*='back-button']").click();
+
+        $$("[class*='switcher_tab'] span")
+                .findBy(Condition.text("С собой"))
+                .shouldBe(visible).click();
+
+        $("[class*='selfdelivery-districts-modal'] h3").shouldHave(text(cityName));
+
+        $$("[class*='list_container'] li [data-sentry-component='DistrictElement']").first().click();
+
+        $("[data-testid='pizza']").click();
+
+        $$("#pizza article[role='button']")
+                .findBy(text(itemPizza))
+                .scrollIntoView((instant().block(center)))
+                .shouldBe(visible).click();
+
+        $$("[class*='editor-buttons_editor']")
+                .findBy(Condition.text("В корзину"))
+                .shouldBe(visible).click();
+
+        actions().sendKeys(Keys.ESCAPE).perform();
+
+        $("button [class*='current-user-location']").click();
+
+        $$("[class*='list_container'] button")
+                .findBy(Condition.text(cityName))
+                .parent()       // поднялись к родителю
+                .sibling(0)     // перешли к следующему контейнеру
+                .$("button")
+                .click();
+
+        $("[class*='entry-card_info']").shouldHave(text("Мясное плато большая"));
+    }
+}
